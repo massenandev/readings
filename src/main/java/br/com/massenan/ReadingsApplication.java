4ef07616671e25f66e33b593f003b7a0df8c017e
Leticia Massenan <massenandev@gmail.com>
2020-07-21 12:34:15 -0300

    setting initial artifacts

---
+++ src/main/java/br/com/massenan/ReadingsApplication.java
@@ -0,0 +1,13 @@
+package br.com.massenan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingsApplication.class, args);
+	}
+
+}
